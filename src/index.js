a9b71ff474f05e675b87438ea3a558230642d27f
PL-coding <113569279+PL-coding@users.noreply.github.com>
2023-01-16 11:40:31 +1100

    Added footer, footer styling and changed page title

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Weather from './Weather';
+import "./index.css";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Weather />
+    <footer>Coded by Louise Gray and open-sourced on <a href='https://github.com/PL-coding/weather-react-app' target="_blank" rel="noreferrer"> GitHub</a></footer>
   </React.StrictMode>
 );
 

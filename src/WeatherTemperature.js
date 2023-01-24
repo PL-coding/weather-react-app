7c85efc2960eb133e65abbb91661ca7153fca00e
PL-coding <113569279+PL-coding@users.noreply.github.com>
2023-01-24 15:21:12 +1100

    Added unit conversion to Fahrenheit

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
+    let [unit, setUnit] = useState("metric");
+    function showFahrenheit(event) {
+        event.preventDefault();
+        setUnit("imperial");
+    }
+    function showCelsius(event) {
+        event.preventDefault();
+        setUnit("metric");
+    }
+    if (unit === "metric") {
     return (
-    <div className="temperature">
-        {Math.round(props.celsius)}<span className="unit"><a href="">°C</a> | <a href="">°F</a></span>
-        </div>)
+    <div className="WeatherTemperature">
+        {Math.round(props.celsius)}<span className="unit">°C | <a href="/" onClick={showFahrenheit}>°F</a></span>
+        </div>);}
+else {
+    return (
+        <div className="WeatherTemperature">
+            {Math.round((props.celsius*1.8) + 32)}<span className="unit"><a href="/" onClick={showCelsius}>°C</a> | °F</span>
+            </div>);
+}
 }
